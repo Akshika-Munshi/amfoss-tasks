bf6c3618c6bdb953ab03285328b2a3409f520361
Akshika-Munshi <116476084+Akshika-Munshi@users.noreply.github.com>
2022-12-14 14:26:59 +0530

    Add files via upload
    The 5th task is in jpeg formal since i had done it in college on my phone and forgot to copy the code to my laptop . Kindly excuse .

---
+++ Task 15/Multiplesof3and5.py
@@ -0,0 +1,17 @@
+t=int(input())                                    
+s=0
+for i in range(1,t+1):
+    n=int(input())
+    for j in range(1,n):
+        if (j%3==0 or j%5==0):
+            s+=j
+    print(s)
+    s=0t=int(input())                                    
+s=0
+for i in range(1,t+1):
+    n=int(input())
+    for j in range(1,n):
+        if (j%3==0 or j%5==0):
+            s+=j
+    print(s)
+    s=0
